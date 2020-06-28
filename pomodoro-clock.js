0cce66277aae5c71e567f4981f1305aa8b9c3896
Malina Randrianavony <malinar@yahoo.com>
2020-06-28 17:12:48 -0600

    Added the ability to decrement/increment sessions' length.

--- pomodoro-clock.js
+++ pomodoro-clock.js
@@ -10,16 +10,29 @@ class PomodoroClock extends React.Component {
                 { name: 'session', timeDefaultVal: 25 },
                 { name: 'break', timeDefaultVal: 5 },
             ],
-            'currSess': { name: 'session', min: 25, sec: 0 }
+            'currSess': { iSess: 0, name: 'session', min: 25, sec: 0 }
         };
+        this.unaryUpdate = this.unaryUpdate.bind(this);
     }
+
+    unaryUpdate(op, iSess) {
+        let sessions = this.state.sessions;
+        if ( op === "-") {
+            sessions[iSess].timeDefaultVal--;
+        }
+        else if ( op === "+") {
+            sessions[iSess].timeDefaultVal++;
+        }
+        this.setState((state, props) => { return {sessions: sessions} });
+    }
+
     render() {
         return (
             <React.Fragment>
                 <Timer currSess={this.state.currSess} />
                 <div className="fl-r">
                     {this.state.sessions.map( (session, i) => (
-                        <Setting key={i} session={session} />
+                        <Setting key={i} session={session} iSess={i} unaryUpdate={this.unaryUpdate} />
                     ))}
                 </div>
             </React.Fragment>
@@ -48,9 +61,9 @@ function Setting(props) {
         <div className="marg-bot-m">
             <div id={props.session.name + "-label"} className="cap">{props.session.name} length</div>
             <div>
-                <span id={props.session.name + "-decrement"} className="unary-button inl-bl">-</span>
+                <button id={props.session.name + "-decrement" } className="unary-button inl-bl pointer" onClick={() => props.unaryUpdate('-', props.iSess)} disabled={props.session.timeDefaultVal<=1} >-</button>
                 <span id={props.session.name + "-length"}>{props.session.timeDefaultVal}</span>
-                <span id={props.session.name + "-increment"} className="unary-button inl-bl">+</span>
+                <button id={props.session.name + "-increment" } className="unary-button inl-bl pointer" onClick={() => props.unaryUpdate('+', props.iSess)} disabled={props.session.timeDefaultVal>=60} >+</button>
             </div>
         </div>
     );
